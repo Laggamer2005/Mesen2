6f6eee9eb62e392b5420085fe03aac286ebbac44
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 19:53:20 +0100

    Update CdlManager.h

--- Core/Debugger/CdlManager.h
+++ Core/Debugger/CdlManager.h
@@ -8,28 +8,28 @@ class CodeDataLogger;
 class Debugger;
 class Disassembler;
 
-class CdlManager
-{
+class CdlManager {
 private:
-	CodeDataLogger* _codeDataLoggers[DebugUtilities::GetMemoryTypeCount()] = {};
-	Debugger* _debugger = nullptr;
-	Disassembler* _disassembler = nullptr;
+    std::array<CodeDataLogger*, DebugUtilities::GetMemoryTypeCount()> _codeDataLoggers{};
+    Debugger* _debugger = nullptr;
+    Disassembler* _disassembler = nullptr;
 
 public:
-	CdlManager(Debugger* debugger, Disassembler* disassembler);
+    CdlManager(Debugger* debugger, Disassembler* disassembler)
+        : _debugger(debugger), _disassembler(disassembler) {}
 
-	void GetCdlData(uint32_t offset, uint32_t length, MemoryType memoryType, uint8_t* cdlData);
-	int16_t GetCdlFlags(MemoryType memType, uint32_t addr);
-	void SetCdlData(MemoryType memType, uint8_t* cdlData, uint32_t length);
-	void MarkBytesAs(MemoryType memType, uint32_t start, uint32_t end, uint8_t flags);
-	CdlStatistics GetCdlStatistics(MemoryType memType);
-	uint32_t GetCdlFunctions(MemoryType memType, uint32_t functions[], uint32_t maxSize);
-	void ResetCdl(MemoryType memType);
-	void LoadCdlFile(MemoryType memType, char* cdlFile);
-	void SaveCdlFile(MemoryType memType, char* cdlFile);
-	void RegisterCdl(MemoryType memType, CodeDataLogger* cdl);
+    void GetCdlData(uint32_t offset, uint32_t length, MemoryType memoryType, uint8_t* cdlData);
+    int16_t GetCdlFlags(MemoryType memType, uint32_t addr);
+    void SetCdlData(MemoryType memType, uint8_t* cdlData, uint32_t length);
+    void MarkBytesAs(MemoryType memType, uint32_t start, uint32_t end, uint8_t flags);
+    CdlStatistics GetCdlStatistics(MemoryType memType);
+    uint32_t GetCdlFunctions(MemoryType memType, uint32_t functions[], uint32_t maxSize);
+    void ResetCdl(MemoryType memType);
+    void LoadCdlFile(MemoryType memType, const char* cdlFile);
+    void SaveCdlFile(MemoryType memType, const char* cdlFile);
+    void RegisterCdl(MemoryType memType, CodeDataLogger* cdl);
 
-	void RefreshCodeCache();
+    void RefreshCodeCache();
 
-	CodeDataLogger* GetCodeDataLogger(MemoryType memType);
+    CodeDataLogger* GetCodeDataLogger(MemoryType memType);
 };
