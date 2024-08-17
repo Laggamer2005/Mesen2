201b8b97c006bd45e688f4dd74ede14bd0dc814b
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:11:48 +0100

    Update FrozenAddressManager.h

--- Core/Debugger/FrozenAddressManager.h
+++ Core/Debugger/FrozenAddressManager.h
@@ -1,34 +1,28 @@
 #pragma once
 #include "pch.h"
 
-class FrozenAddressManager
-{
-protected:
-	unordered_set<uint32_t> _frozenAddresses;
+class FrozenAddressManager {
+private:
+    std::unordered_set<uint32_t> _frozenAddresses;
 
 public:
-	void UpdateFrozenAddresses(uint32_t start, uint32_t end, bool freeze)
-	{
-		if(freeze) {
-			for(uint32_t i = start; i <= end; i++) {
-				_frozenAddresses.emplace(i);
-			}
-		} else {
-			for(uint32_t i = start; i <= end; i++) {
-				_frozenAddresses.erase(i);
-			}
-		}
-	}
+    void UpdateFrozenAddresses(uint32_t start, uint32_t end, bool freeze) {
+        if (freeze) {
+            _frozenAddresses.insert(start, end + 1);
+        } else {
+            for (uint32_t i = start; i <= end; i++) {
+                _frozenAddresses.erase(i);
+            }
+        }
+    }
 
-	bool IsFrozenAddress(uint32_t addr)
-	{
-		return _frozenAddresses.size() > 0 && _frozenAddresses.find(addr) != _frozenAddresses.end();
-	}
+    bool IsFrozenAddress(uint32_t addr) const {
+        return _frozenAddresses.find(addr) != _frozenAddresses.end();
+    }
 
-	void GetFrozenState(uint32_t start, uint32_t end, bool* outState)
-	{
-		for(uint32_t i = start; i <= end; i++) {
-			outState[i - start] = _frozenAddresses.find(i) != _frozenAddresses.end();
-		}
-	}
+    void GetFrozenState(uint32_t start, uint32_t end, bool* outState) const {
+        for (uint32_t i = start; i <= end; i++) {
+            outState[i_-_start] = IsFrozenAddress(i);
+        }
+    }
 };
