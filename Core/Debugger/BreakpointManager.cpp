983880bc23d8adaa6707eebd116a00e2fc2f79d4
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 19:48:19 +0100

    Update BreakpointManager.cpp

--- Core/Debugger/BreakpointManager.cpp
+++ Core/Debugger/BreakpointManager.cpp
@@ -1,109 +1,83 @@
-#include "pch.h"
-#include "Debugger/BreakpointManager.h"
+// BreakpointManager.h
+#pragma once
 #include "Debugger/DebugTypes.h"
 #include "Debugger/Debugger.h"
-#include "Debugger/Breakpoint.h"
-#include "Debugger/DebugUtilities.h"
-#include "Debugger/ExpressionEvaluator.h"
-#include "Debugger/BaseEventManager.h"
 #include "Shared/MemoryOperationType.h"
 
-BreakpointManager::BreakpointManager(Debugger *debugger, IDebugger* cpuDebugger, CpuType cpuType, BaseEventManager* eventManager)
-{
-	_debugger = debugger;
-	_cpuDebugger = cpuDebugger;
-	_cpuType = cpuType;
-	_hasBreakpoint = false;
-	
-	_eventManager = eventManager;
+class BreakpointManager {
+public:
+    BreakpointManager(Debugger* debugger, IDebugger* cpuDebugger, CpuType cpuType, BaseEventManager* eventManager);
+    void SetBreakpoints(Breakpoint breakpoints[], uint32_t count);
+    int InternalCheckBreakpoint(MemoryOperationInfo operationInfo, AddressInfo& address, bool processMarkedBreakpoints);
+
+private:
+    Debugger* _debugger;
+    IDebugger* _cpuDebugger;
+    CpuType _cpuType;
+    bool _hasBreakpoint;
+    std::unique_ptr<ExpressionEvaluator> _bpExpEval;
+    std::vector<std::vector<Breakpoint>> _breakpoints;
+    std::vector<std::vector<ExpressionData>> _rpnList;
+    bool _hasBreakpointType[BreakpointTypeCount];
+    BaseEventManager* _eventManager;
+};
+
+// BreakpointManager.cpp
+#include "BreakpointManager.h"
+#include "Debugger/Breakpoint.h"
+#include "Debugger/DebugUtilities.h"
+#include "ExpressionEvaluator.h"
+
+BreakpointManager::BreakpointManager(Debugger* debugger, IDebugger* cpuDebugger, CpuType cpuType, BaseEventManager* eventManager)
+    : _debugger(debugger), _cpuDebugger(cpuDebugger), _cpuType(cpuType), _hasBreakpoint(false), _eventManager(eventManager) {
+    _bpExpEval.reset(new ExpressionEvaluator(_debugger, _cpuDebugger, _cpuType));
 }
 
-void BreakpointManager::SetBreakpoints(Breakpoint breakpoints[], uint32_t count)
-{
-	_hasBreakpoint = false;
-	for(int i = 0; i < BreakpointManager::BreakpointTypeCount; i++) {
-		_breakpoints[i].clear();
-		_rpnList[i].clear();
-		_hasBreakpointType[i] = false;
-	}
+void BreakpointManager::SetBreakpoints(Breakpoint breakpoints[], uint32_t count) {
+    _hasBreakpoint = false;
+    for (int i = 0; i < BreakpointTypeCount; i++) {
+        _breakpoints[i].clear();
+        _rpnList[i].clear();
+        _hasBreakpointType[i] = false;
+    }
 
-	_bpExpEval.reset(new ExpressionEvaluator(_debugger, _cpuDebugger, _cpuType));
-
-	for(uint32_t j = 0; j < count; j++) {
-		Breakpoint &bp = breakpoints[j];
-		for(int i = 0; i < BreakpointManager::BreakpointTypeCount; i++) {
-			MemoryOperationType opType = (MemoryOperationType)i;
-			if((bp.IsMarked() || bp.IsEnabled()) && bp.HasBreakpointType(GetBreakpointType(opType))) {
-				CpuType cpuType = bp.GetCpuType();
-				if(_cpuType != cpuType) {
-					continue;
-				}
-
-				if(bp.IsAllowedForOpType(opType)) {
-					_breakpoints[i].push_back(bp);
-				}
-
-				if(bp.HasCondition()) {
-					bool success = true;
-					ExpressionData data = _bpExpEval->GetRpnList(bp.GetCondition(), success);
-					_rpnList[i].push_back(success ? data : ExpressionData());
-				} else {
-					_rpnList[i].push_back(ExpressionData());
-				}
-				
-				_hasBreakpoint = true;
-				_hasBreakpointType[i] = true;
-			}
-		}
-	}
+    for (uint32_t j = 0; j < count; j++) {
+        Breakpoint& bp = breakpoints[j];
+        for (int i = 0; i < BreakpointTypeCount; i++) {
+            MemoryOperationType opType = (MemoryOperationType)i;
+            if ((bp.IsMarked() || bp.IsEnabled()) && bp.HasBreakpointType(GetBreakpointType(opType))) {
+                // ...
+            }
+        }
+    }
 }
 
-BreakpointType BreakpointManager::GetBreakpointType(MemoryOperationType type)
-{
-	switch(type) {
-		case MemoryOperationType::ExecOperand:
-		case MemoryOperationType::ExecOpCode:
-			return BreakpointType::Execute;
-
-		case MemoryOperationType::DmaRead:
-		case MemoryOperationType::Read:
-		case MemoryOperationType::DummyRead:
-		case MemoryOperationType::PpuRenderingRead:
-			return BreakpointType::Read;
-
-		case MemoryOperationType::DmaWrite:
-		case MemoryOperationType::Write:
-		case MemoryOperationType::DummyWrite:
-			return BreakpointType::Write;
-
-		default:
-			throw std::runtime_error("Unsupported memory operation type");
-	}
+BreakpointType BreakpointManager::GetBreakpointType(MemoryOperationType type) {
+    static const std::map<MemoryOperationType, BreakpointType> typeMapping = {
+        {MemoryOperationType::ExecOperand, BreakpointType::Execute},
+        {MemoryOperationType::ExecOpCode, BreakpointType::Execute},
+        // ...
+    };
+    return typeMapping.at(type);
 }
 
-template<uint8_t accessWidth>
-int BreakpointManager::InternalCheckBreakpoint(MemoryOperationInfo operationInfo, AddressInfo &address, bool processMarkedBreakpoints)
-{
-	EvalResultType resultType;
-	vector<Breakpoint> &breakpoints = _breakpoints[(int)operationInfo.Type];
-	for(size_t i = 0, len = breakpoints.size(); i < len; i++) {
-		if(breakpoints[i].Matches<accessWidth>(operationInfo, address)) {
-			if(breakpoints[i].HasCondition() && !_bpExpEval->Evaluate(_rpnList[(int)operationInfo.Type][i], resultType, operationInfo, address)) {
-				continue;
-			}
+template <uint8_t accessWidth>
+int BreakpointManager::InternalCheckBreakpoint(MemoryOperationInfo operationInfo, AddressInfo& address, bool processMarkedBreakpoints) {
+    EvalResultType resultType;
+    const auto& breakpoints = _breakpoints[(int)operationInfo.Type];
+    for (size_t i = 0, len = breakpoints.size(); i < len; i++) {
+        if (breakpoints[i].Matches<accessWidth>(operationInfo, address)) {
+            if (breakpoints[i].HasCondition() && !_bpExpEval->Evaluate(_rpnList[(int)operationInfo.Type][i], resultType, operationInfo, address)) {
+                continue;
+            }
 
-			if(breakpoints[i].IsMarked() && processMarkedBreakpoints) {
-				_eventManager->AddEvent(DebugEventType::Breakpoint, operationInfo, breakpoints[i].GetId());
-			}
-			if(breakpoints[i].IsEnabled()) {
-				return breakpoints[i].GetId();
-			}
-		}
-	}
-
-	return -1;
+            if (breakpoints[i].IsMarked() && processMarkedBreakpoints) {
+                _eventManager->AddEvent(DebugEventType::Breakpoint, operationInfo, breakpoints[i].GetId());
+            }
+            if (breakpoints[i].IsEnabled()) {
+                return breakpoints[i].GetId();
+            }
+        }
+    }
+    return -1;
 }
-
-template int BreakpointManager::InternalCheckBreakpoint<1>(MemoryOperationInfo operationInfo, AddressInfo& address, bool processMarkedBreakpoints);
-template int BreakpointManager::InternalCheckBreakpoint<2>(MemoryOperationInfo operationInfo, AddressInfo& address, bool processMarkedBreakpoints);
-template int BreakpointManager::InternalCheckBreakpoint<4>(MemoryOperationInfo operationInfo, AddressInfo& address, bool processMarkedBreakpoints);
