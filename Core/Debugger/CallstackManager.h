7da6c5d58bcded8464335c67d0408976655cad2d
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 19:51:32 +0100

    Update CallstackManager.h

--- Core/Debugger/CallstackManager.h
+++ Core/Debugger/CallstackManager.h
@@ -6,38 +6,31 @@ class Debugger;
 class Profiler;
 class IConsole;
 
-class CallstackManager
-{
+class CallstackManager {
 private:
-	Debugger* _debugger;
-	deque<StackFrameInfo> _callstack;
-	unique_ptr<Profiler> _profiler;
+    Debugger* _debugger;
+    std::deque<StackFrameInfo> _callstack;
+    std::unique_ptr<Profiler> _profiler;
 
 public:
-	CallstackManager(Debugger* debugger, IConsole* console);
-	~CallstackManager();
+    CallstackManager(Debugger* debugger, IConsole* console);
+    ~CallstackManager();
 
-	void Push(AddressInfo& src, uint32_t srcAddr, AddressInfo& dest, uint32_t destAddr, AddressInfo& ret, uint32_t returnAddress, StackFrameFlags flags);
-	void Pop(AddressInfo& dest, uint32_t destAddr);
+    void Push(AddressInfo& src, uint32_t srcAddr, AddressInfo& dest, uint32_t destAddr, AddressInfo& ret, uint32_t returnAddress, StackFrameFlags flags);
+    void Pop(AddressInfo& dest, uint32_t destAddr);
 
-	__forceinline bool IsReturnAddrMatch(uint32_t destAddr)
-	{
-		if(_callstack.empty()) {
-			return false;
-		}
+    [[nodiscard]] bool IsReturnAddrMatch(uint32_t destAddr) const noexcept {
+        for (auto it = _callstack.rbegin(); it != _callstack.rend(); ++it) {
+            if (it->Return == destAddr) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-		for(auto itt = _callstack.rbegin(); itt != _callstack.rend(); itt++) {
-			if((*itt).Return == destAddr) {
-				return true;
-			}
-		}
+    void GetCallstack(StackFrameInfo* callstackArray, uint32_t& callstackSize);
+    int32_t GetReturnAddress();
+    Profiler* GetProfiler();
 
-		return false;
-	}
-
-	void GetCallstack(StackFrameInfo* callstackArray, uint32_t &callstackSize);
-	int32_t GetReturnAddress();
-	Profiler* GetProfiler();
-
-	void Clear();
+    void Clear();
 };
