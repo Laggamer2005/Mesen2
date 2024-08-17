77bc85327d6cb79bf83d64f560346bcbf19cbe97
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:01:47 +0100

    Update DebuggerFeatures.h

--- Core/Debugger/DebuggerFeatures.h
+++ Core/Debugger/DebuggerFeatures.h
@@ -1,30 +1,27 @@
 #pragma once
 #include "pch.h"
 
-enum class VectorType
-{
-	Indirect,
-	Direct,
+enum class VectorType : uint8_t { // Use uint8_t as the underlying type to save space
+    Indirect,
+    Direct,
 };
 
-struct CpuVectorDefinition
-{
-	char Name[15] = {};
-	uint32_t Address = 0;
-	VectorType Type = VectorType::Indirect;
+struct CpuVectorDefinition {
+    char Name[15] = {}; // Consider using std::string or const char* instead of a fixed-size array
+    uint32_t Address = 0;
+    VectorType Type = VectorType::Indirect;
 };
 
-struct DebuggerFeatures
-{
-	bool RunToIrq;
-	bool RunToNmi;
-	bool StepOver;
-	bool StepOut;
-	bool StepBack;
-	bool ChangeProgramCounter;
-	bool CallStack;
-	bool CpuCycleStep;
-	
-	uint8_t CpuVectorCount;
-	CpuVectorDefinition CpuVectors[10];
+struct DebuggerFeatures {
+    bool RunToIrq : 1; // Use bit fields to save space
+    bool RunToNmi : 1;
+    bool StepOver : 1;
+    bool StepOut : 1;
+    bool StepBack : 1;
+    bool ChangeProgramCounter : 1;
+    bool CallStack : 1;
+    bool CpuCycleStep : 1;
+
+    uint8_t CpuVectorCount;
+    CpuVectorDefinition CpuVectors[10]; // Consider using a std::vector instead of a fixed-size array
 };
