a6cf4da12d87b845d1a23998e7da8252145dae7c
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:08:30 +0100

    Update DisassemblySearch.h

--- Core/Debugger/DisassemblySearch.h
+++ Core/Debugger/DisassemblySearch.h
@@ -8,29 +8,26 @@ class Disassembler;
 class LabelManager;
 enum class CpuType : uint8_t;
 
-struct DisassemblySearchOptions
-{
-	bool MatchCase;
-	bool MatchWholeWord;
-	bool SearchBackwards;
-	bool SkipFirstLine;
+struct DisassemblySearchOptions {
+    bool matchCase;
+    bool matchWholeWord;
+    bool searchBackwards;
+    bool skipFirstLine;
 };
 
-class DisassemblySearch
-{
+class DisassemblySearch {
 private:
-	Disassembler* _disassembler;
-	LabelManager* _labelManager;
+    Disassembler* disassembler;
+    LabelManager* labelManager;
 
-	uint32_t SearchDisassembly(CpuType cpuType, const char* searchString, int32_t startAddress, DisassemblySearchOptions options, CodeLineData searchResults[], uint32_t maxResultCount);
+    uint32_t searchDisassembly(CpuType cpuType, const char* searchString, int32_t startAddress, DisassemblySearchOptions options, CodeLineData searchResults[], uint32_t maxResultCount);
 
-	template<bool matchCase> bool TextContains(string& needle, const char* hay, int size, DisassemblySearchOptions& options);
-	bool TextContains(string& needle, const char* hay, int size, DisassemblySearchOptions& options);
-	bool IsWordSeparator(char c);
+    bool textContains(const std::string& needle, const char* hay, int size, DisassemblySearchOptions& options) const;
+    bool isWordSeparator(char c) const;
 
 public:
-	DisassemblySearch(Disassembler* disassembler, LabelManager* labelManager);
+    DisassemblySearch(Disassembler* disassembler, LabelManager* labelManager);
 
-	int32_t SearchDisassembly(CpuType cpuType, const char* searchString, int32_t startAddress, DisassemblySearchOptions options);
-	uint32_t FindOccurrences(CpuType cpuType, const char* searchString, DisassemblySearchOptions options, CodeLineData output[], uint32_t maxResultCount);
+    int32_t searchDisassembly(CpuType cpuType, const char* searchString, int32_t startAddress, DisassemblySearchOptions options);
+    uint32_t findOccurrences(CpuType cpuType, const char* searchString, DisassemblySearchOptions options, CodeLineData output[], uint32_t maxResultCount);
 };
