9fc2717cbad4a32d750cdff26b1fd311df0c1821
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:29:19 +0100

    Update Profiler.cpp

--- Core/Debugger/Profiler.cpp
+++ Core/Debugger/Profiler.cpp
@@ -10,127 +10,104 @@
 static constexpr int32_t ResetFunctionIndex = -1;
 
 Profiler::Profiler(Debugger* debugger, IConsole* console)
-{
-	_debugger = debugger;
-	_console = console;
-	InternalReset();
+    : _debugger(debugger), _console(console) {
+    InternalReset();
 }
 
-Profiler::~Profiler()
-{
+Profiler::~Profiler() {}
+
+void Profiler::StackFunction(AddressInfo& addr, StackFrameFlags stackFlag) {
+    if (addr.Address >= 0) {
+        uint32_t key = addr.Address | ((uint8_t)addr.Type << 24);
+        if (_functions.find(key) == _functions.end()) {
+            _functions[key] = ProfiledFunction();
+            _functions[key].Address = addr;
+        }
+
+        UpdateCycles();
+
+        _stackFlags.emplace_back(stackFlag);
+        _cycleCountStack.emplace_back(_currentCycleCount);
+        _functionStack.emplace_back(_currentFunction);
+
+        if (_functionStack.size() > 100) {
+            _functionStack.pop_front();
+            _cycleCountStack.pop_front();
+            _stackFlags.pop_front();
+        }
+
+        _functions[key].CallCount++;
+
+        _currentFunction = key;
+        _currentCycleCount = 0;
+    }
 }
 
-void Profiler::StackFunction(AddressInfo &addr, StackFrameFlags stackFlag)
-{
-	if(addr.Address >= 0) {
-		uint32_t key = addr.Address | ((uint8_t)addr.Type << 24);
-		if(_functions.find(key) == _functions.end()) {
-			_functions[key] = ProfiledFunction();
-			_functions[key].Address = addr;
-		}
+void Profiler::UpdateCycles() {
+    uint64_t masterClock = _console->GetMasterClock();
+    uint64_t clockGap = masterClock - _prevMasterClock;
 
-		UpdateCycles();
+    ProfiledFunction& func = _functions[_currentFunction];
+    func.ExclusiveCycles += clockGap;
+    func.InclusiveCycles += clockGap;
 
-		_stackFlags.push_back(stackFlag);
-		_cycleCountStack.push_back(_currentCycleCount);
-		_functionStack.push_back(_currentFunction);
+    for (auto it = _functionStack.rbegin(); it != _functionStack.rend(); ++it) {
+        _functions[*it].InclusiveCycles += clockGap;
+        if (_stackFlags[_functionStack.size()_-_1_-_std::distance(_functionStack.begin(),_it)] != StackFrameFlags::None) {
+            break;
+        }
+    }
 
-		if(_functionStack.size() > 100) {
-			//Keep stack to 100 functions at most (to prevent performance issues, esp. in debug builds)
-			//Only happens when software doesn't use JSR/RTS normally to enter/leave functions
-			_functionStack.pop_front();
-			_cycleCountStack.pop_front();
-			_stackFlags.pop_front();
-		}
-
-		ProfiledFunction& func = _functions[key];
-		func.CallCount++;
-
-		_currentFunction = key;
-		_currentCycleCount = 0;
-	}
+    _currentCycleCount += clockGap;
+    _prevMasterClock = masterClock;
 }
 
-void Profiler::UpdateCycles()
-{
-	uint64_t masterClock = _console->GetMasterClock();
-	
-	ProfiledFunction& func = _functions[_currentFunction];
-	uint64_t clockGap = masterClock - _prevMasterClock;
-	func.ExclusiveCycles += clockGap;
-	func.InclusiveCycles += clockGap;
-	
-	int32_t len = (int32_t)_functionStack.size();
-	for(int32_t i = len - 1; i >= 0; i--) {
-		_functions[_functionStack[i]].InclusiveCycles += clockGap;
-		if(_stackFlags[i] != StackFrameFlags::None) {
-			//Don't apply inclusive times to stack frames before an IRQ/NMI
-			break;
-		}
-	}
+void Profiler::UnstackFunction() {
+    if (!_functionStack.empty()) {
+        UpdateCycles();
 
-	_currentCycleCount += clockGap;
-	_prevMasterClock = masterClock;
+        ProfiledFunction& func = _functions[_currentFunction];
+        func.MinCycles = std::min(func.MinCycles, _currentCycleCount);
+        func.MaxCycles = std::max(func.MaxCycles, _currentCycleCount);
+
+        _currentFunction = _functionStack.back();
+        _functionStack.pop_back();
+        _stackFlags.pop_back();
+        _currentCycleCount = _cycleCountStack.back() + _currentCycleCount;
+        _cycleCountStack.pop_back();
+    }
 }
 
-void Profiler::UnstackFunction()
-{
-	if(!_functionStack.empty()) {
-		UpdateCycles();
-
-		//Return to the previous function
-		ProfiledFunction& func = _functions[_currentFunction];
-		func.MinCycles = std::min(func.MinCycles, _currentCycleCount);
-		func.MaxCycles = std::max(func.MaxCycles, _currentCycleCount);
-
-		_currentFunction = _functionStack.back();
-		_functionStack.pop_back();
-		_stackFlags.pop_back();
-
-		//Add the subroutine's cycle count to the current routine's cycle count
-		_currentCycleCount = _cycleCountStack.back() + _currentCycleCount;
-		_cycleCountStack.pop_back();
-	}
+void Profiler::Reset() {
+    DebugBreakHelper helper(_debugger);
+    InternalReset();
 }
 
-void Profiler::Reset()
-{
-	DebugBreakHelper helper(_debugger);
-	InternalReset();
+void Profiler::ResetState() {
+    _prevMasterClock = _console->GetMasterClock();
+    _currentCycleCount = 0;
+    _functionStack.clear();
+    _stackFlags.clear();
+    _cycleCountStack.clear();
+    _currentFunction = ResetFunctionIndex;
 }
 
-void Profiler::ResetState()
-{
-	_prevMasterClock = _console->GetMasterClock();
-	_currentCycleCount = 0;
-	_functionStack.clear();
-	_stackFlags.clear();
-	_cycleCountStack.clear();
-	_currentFunction = ResetFunctionIndex;
+void Profiler::InternalReset() {
+    ResetState();
+    _functions.clear();
+    _functions[ResetFunctionIndex] = ProfiledFunction();
+    _functions[ResetFunctionIndex].Address = { ResetFunctionIndex, MemoryType::None };
 }
 
-void Profiler::InternalReset()
-{
-	ResetState();
-	
-	_functions.clear();
-	_functions[ResetFunctionIndex] = ProfiledFunction();
-	_functions[ResetFunctionIndex].Address = { ResetFunctionIndex, MemoryType::None };
-}
+void Profiler::GetProfilerData(ProfiledFunction* profilerData, uint32_t& functionCount) {
+    DebugBreakHelper helper(_debugger);
 
-void Profiler::GetProfilerData(ProfiledFunction* profilerData, uint32_t& functionCount)
-{
-	DebugBreakHelper helper(_debugger);
-	
-	UpdateCycles();
+    UpdateCycles();
 
-	functionCount = 0;
-	for(auto& func : _functions) {
-		profilerData[functionCount] = func.second;
-		functionCount++;
+    functionCount = 0;
+    for (auto& func : _functions) {
+        profilerData[functionCount] = func.second;
+        functionCount++;
 
-		if(functionCount >= 100000) {
-			break;
-		}
-	}
-}
+        if (functionCount >= 100000) {
+            break;
