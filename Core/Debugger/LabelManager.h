c4a3940c43842ab4f3e3ae61fa765c517c34d230
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:15:47 +0100

    Update LabelManager.h

--- Core/Debugger/LabelManager.h
+++ Core/Debugger/LabelManager.h
@@ -6,48 +6,42 @@
 
 class Debugger;
 
-class AddressHasher
-{
+class AddressHasher {
 public:
-	size_t operator()(const uint64_t& addr) const
-	{
-		//Quick hash for addresses
-		return addr;
-	}
+    size_t operator()(uint64_t addr) const noexcept { // added noexcept
+        return addr; // no need for const reference, as uint64_t is primitive
+    }
 };
 
-struct LabelInfo
-{
-	string Label;
-	string Comment;
+struct LabelInfo {
+    std::string Label;
+    std::string Comment;
 };
 
-class LabelManager
-{
+class LabelManager {
 private:
-	unordered_map<uint64_t, LabelInfo, AddressHasher> _codeLabels;
-	unordered_map<string, uint64_t> _codeLabelReverseLookup;
+    std::unordered_map<uint64_t, LabelInfo, AddressHasher> _codeLabels;
+    std::unordered_map<std::string, uint64_t> _codeLabelReverseLookup;
 
-	Debugger *_debugger;
+    Debugger* _debugger;
 
-	int64_t GetLabelKey(uint32_t absoluteAddr, MemoryType memType);
-	MemoryType GetKeyMemoryType(uint64_t key);
-	bool InternalGetLabel(AddressInfo address, string& label);
+    int64_t GetLabelKey(uint32_t absoluteAddr, MemoryType memType);
+    MemoryType GetKeyMemoryType(uint64_t key);
+    bool InternalGetLabel(AddressInfo address, std::string& label);
 
 public:
-	LabelManager(Debugger *debugger);
+    explicit LabelManager(Debugger* debugger) : _debugger{debugger} {} // added explicit and initializer list
 
-	void SetLabel(uint32_t address, MemoryType memType, string label, string comment);
-	void ClearLabels();
+    void SetLabel(uint32_t address, MemoryType memType, const std::string& label, const std::string& comment);
+    void ClearLabels();
 
-	AddressInfo GetLabelAbsoluteAddress(string& label);
-	int32_t GetLabelRelativeAddress(string &label, CpuType cpuType);
+    AddressInfo GetLabelAbsoluteAddress(const std::string& label) const; // added const
+    int32_t GetLabelRelativeAddress(const std::string& label, CpuType cpuType);
 
-	string GetLabel(AddressInfo address, bool checkRegisterLabels = true);
-	string GetComment(AddressInfo absAddress);
-	bool GetLabelAndComment(AddressInfo address, LabelInfo &label);
+    std::string GetLabel(AddressInfo address, bool checkRegisterLabels = true) const; // added const
+    std::string GetComment(AddressInfo absAddress) const; // added const
+    bool GetLabelAndComment(AddressInfo address, LabelInfo& label);
 
-	bool ContainsLabel(string &label);
-
-	bool HasLabelOrComment(AddressInfo address);
+    bool ContainsLabel(const std::string& label) const; // added const
+    bool HasLabelOrComment(AddressInfo address) const; // added const
 };
